09636b0bde096fa56f161abe7f59cf0fcdb7a0bc
Koczokok <p.kurylowicz@outlook.com>
2023-06-11 12:18:04 +0200

    regiter

--- src/main/webapp/app/account/register/register.model.ts
+++ src/main/webapp/app/account/register/register.model.ts
@@ -1,3 +1,9 @@
-export class Registration {
+export class Registration1 {
   constructor(public login: string, public email: string, public password: string, public langKey: string) {}
 }
+
+export class Registration2 {
+  constructor(public firstName: string, public lastName: string, public prefix: string, public number: string) {}
+}
+export class Registration3 {}
+export class Registration4 {}
--- src/main/webapp/app/account/register/register.service.ts
+++ src/main/webapp/app/account/register/register.service.ts
@@ -3,14 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
-import { Registration } from './register.model';
+import { Registration1 } from './register.model';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
   success = false;
   constructor(private http: HttpClient, private applicationConfigService: ApplicationConfigService) {}
 
-  save(registration: Registration): Observable<{}> {
+  save(registration: Registration1): Observable<{}> {
     return this.http.post(this.applicationConfigService.getEndpointFor('api/register'), registration);
   }
 }
