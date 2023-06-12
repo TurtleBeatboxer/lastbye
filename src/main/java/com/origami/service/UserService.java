8974722a52105fb9697cffe6078dd0c8aefb6b5d
Koczokok <p.kurylowicz@outlook.com>
2023-06-12 10:35:00 +0200

    register

--- src/main/java/com/origami/service/UserService.java
+++ src/main/java/com/origami/service/UserService.java
@@ -106,6 +106,7 @@ public class UserService {
     }
 
     public User registerUser(ManagedUserVM userDTO, String password) {
+        System.out.println(userDTO);
         userRepository
             .findOneByLogin(userDTO.getLogin().toLowerCase())
             .ifPresent(existingUser -> {
--- src/main/webapp/app/account/register/register.model.ts
+++ src/main/webapp/app/account/register/register.model.ts
@@ -3,7 +3,71 @@ export class Registration1 {
 }
 
 export class Registration2 {
-  constructor(public firstName: string, public lastName: string, public prefix: string, public number: string) {}
+  constructor(public firstName: string, public lastName: string, public prefix: string, public phoneNumber: string) {}
 }
-export class Registration3 {}
-export class Registration4 {}
+export class Registration3 {
+  constructor(
+    public burialMethod: string,
+    public clothes: string,
+    public placeOfCeremony: string,
+    public photo: string,
+    public graveInscription: string,
+    public spotify: string,
+    public guests: string,
+    public notInvited: string,
+    public obituary: string
+  ) {}
+}
+export class Registration4 {
+  constructor(
+    public purchasedPlace: boolean,
+    public isPurchasedOther: string,
+    public flowers: boolean,
+    public ifFlowers: string,
+    public farwellLetter: string,
+    public speech: string,
+    public videoSpeech: string,
+    public testament: string,
+    public accessForRelatives: string,
+    public other: string
+  ) {}
+}
+
+export interface registeration {
+  reg1: Registration1;
+  reg2: Registration2;
+  reg3: Registration3;
+  reg4: Registration4;
+}
+
+export class registeration1 {
+  constructor(
+    public login: string,
+    public email: string,
+    public password: string,
+    public langKey: string,
+    public firstName: string,
+    public lastName: string,
+    public prefix: string,
+    public phoneNumber: string,
+    public burialMethod: string,
+    public clothes: string,
+    public placeOfCeremony: string,
+    public photo: string,
+    public graveInscription: string,
+    public spotify: string,
+    public guests: string,
+    public notInvited: string,
+    public obituary: string,
+    public purchasedPlace: boolean,
+    public isPurchasedOther: string,
+    public flowers: boolean,
+    public ifFlowers: string,
+    public farwellLetter: string,
+    public speech: string,
+    public videoSpeech: string,
+    public testament: string,
+    public accessForRelatives: string,
+    public other: string
+  ) {}
+}
