445af3eb8ef098760104abe2ba17c074c446efaa
sztosiek <sztosek99@gmail.com>
2023-06-16 14:39:13 +0200

    updatestrony

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NbAccordionModule } from '@nebular/theme';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), NbAccordionModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
