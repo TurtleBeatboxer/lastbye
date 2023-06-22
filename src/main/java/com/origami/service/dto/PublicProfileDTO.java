91115d0312f0069f0c0c95719944253bcf84d244
mmarzecki <mmarzecki2@finitec.pl>
2023-06-22 17:19:04 +0200

    Skonczony task

---
+++ src/main/java/com/origami/service/dto/PublicProfileDTO.java
@@ -0,0 +1,194 @@
+package com.origami.service.dto;
+
+public class PublicProfileDTO {
+
+    private String firstName;
+    private String lastName;
+    private String burialMethod;
+    private String clothes;
+    private String placeOfCeremony;
+    private String photo;
+    private String graveInscription;
+    private String spotify;
+    private String guests;
+    private String notInvited;
+    private String obituary;
+    private boolean purchasedPlace;
+    private String isPurchasedOther;
+    private boolean flowers;
+    private String ifFlowers;
+    private String farewellLetter;
+    private String speech;
+    private String videoSpeech;
+    private String testament;
+    private String other;
+    private boolean isOpenCoffin;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBurialMethod() {
+        return burialMethod;
+    }
+
+    public void setBurialMethod(String burialMethod) {
+        this.burialMethod = burialMethod;
+    }
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public void setClothes(String clothes) {
+        this.clothes = clothes;
+    }
+
+    public String getPlaceOfCeremony() {
+        return placeOfCeremony;
+    }
+
+    public void setPlaceOfCeremony(String placeOfCeremony) {
+        this.placeOfCeremony = placeOfCeremony;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getGraveInscription() {
+        return graveInscription;
+    }
+
+    public void setGraveInscription(String graveInscription) {
+        this.graveInscription = graveInscription;
+    }
+
+    public String getSpotify() {
+        return spotify;
+    }
+
+    public void setSpotify(String spotify) {
+        this.spotify = spotify;
+    }
+
+    public String getGuests() {
+        return guests;
+    }
+
+    public void setGuests(String guests) {
+        this.guests = guests;
+    }
+
+    public String getNotInvited() {
+        return notInvited;
+    }
+
+    public void setNotInvited(String notInvited) {
+        this.notInvited = notInvited;
+    }
+
+    public String getObituary() {
+        return obituary;
+    }
+
+    public void setObituary(String obituary) {
+        this.obituary = obituary;
+    }
+
+    public boolean isPurchasedPlace() {
+        return purchasedPlace;
+    }
+
+    public void setPurchasedPlace(boolean purchasedPlace) {
+        this.purchasedPlace = purchasedPlace;
+    }
+
+    public String getIsPurchasedOther() {
+        return isPurchasedOther;
+    }
+
+    public void setIsPurchasedOther(String isPurchasedOther) {
+        this.isPurchasedOther = isPurchasedOther;
+    }
+
+    public boolean isFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(boolean flowers) {
+        this.flowers = flowers;
+    }
+
+    public String getIfFlowers() {
+        return ifFlowers;
+    }
+
+    public void setIfFlowers(String ifFlowers) {
+        this.ifFlowers = ifFlowers;
+    }
+
+    public String getFarewellLetter() {
+        return farewellLetter;
+    }
+
+    public void setFarewellLetter(String farewellLetter) {
+        this.farewellLetter = farewellLetter;
+    }
+
+    public String getSpeech() {
+        return speech;
+    }
+
+    public void setSpeech(String speech) {
+        this.speech = speech;
+    }
+
+    public String getVideoSpeech() {
+        return videoSpeech;
+    }
+
+    public void setVideoSpeech(String videoSpeech) {
+        this.videoSpeech = videoSpeech;
+    }
+
+    public String getTestament() {
+        return testament;
+    }
+
+    public void setTestament(String testament) {
+        this.testament = testament;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+    public boolean isOpenCoffin() {
+        return isOpenCoffin;
+    }
+
+    public void setOpenCoffin(boolean openCoffin) {
+        isOpenCoffin = openCoffin;
+    }
+}
