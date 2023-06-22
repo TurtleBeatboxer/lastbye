a03e66d8696a67e6cc23b55b42589f0284c81e8a
mmarzecki <mmarzecki2@finitec.pl>
2023-06-22 17:19:04 +0200

    Skonczony task

--- src/main/java/com/origami/repository/ProfileRepository.java
+++ src/main/java/com/origami/repository/ProfileRepository.java
@@ -13,4 +13,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findOneByUserId(Long userId);
+    Optional<Profile> findProfileByPublicProfileLink(String publicProfileLink);
 }
