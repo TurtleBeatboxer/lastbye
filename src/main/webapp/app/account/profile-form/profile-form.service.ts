8d78d67d1d785d1b955a94a0afe4f083e8f78973
Koczokok <p.kurylowicz@outlook.com>
2023-06-25 12:34:20 +0200

    happy path

--- src/main/webapp/app/account/profile-form/profile-form.service.ts
+++ src/main/webapp/app/account/profile-form/profile-form.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { profileData2, profileForm2 } from './profile-form.model';
+import { profileData2, profileData3, profileData4, profileForm2, profileForm3, profileForm4 } from './profile-form.model';
 import { AccountService } from 'app/core/auth/account.service';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { AccountService } from 'app/core/auth/account.service';
 export class ProfileFormService {
   constructor(private accountService: AccountService) {}
 
-  dataProfile1(form: profileForm2): profileData2 {
+  dataProfile2(form: profileForm2): profileData2 {
     if (this.accountService.userIdentity) {
       return new profileData2(
         form.burialMethod,
@@ -18,10 +18,45 @@ export class ProfileFormService {
         form.clothes,
         this.accountService.userIdentity.userId,
         this.accountService.userIdentity.login,
-        2
+        1
       );
     } else {
       return new profileData2('', '', false, '', -1, '', 0);
+    }
+  }
+
+  dataProfile3(form: profileForm3): profileData3 {
+    if (this.accountService.userIdentity) {
+      return new profileData3(
+        form.flowers,
+        form.ifFlowers,
+        form.obituary,
+        form.spotify,
+        form.guests,
+        form.notInvited,
+        form.placeOfCeremony,
+        this.accountService.userIdentity.userId,
+        this.accountService.userIdentity.login,
+        2
+      );
+    } else {
+      return new profileData3(false, '', '', '', '', '', '', -1, '', 0);
+    }
+  }
+
+  dataProfile4(form: profileForm4): profileData4 {
+    if (this.accountService.userIdentity) {
+      return new profileData4(
+        form.farewellLetter,
+        form.videoSpeech,
+        form.testament,
+        form.other,
+        this.accountService.userIdentity.userId,
+        this.accountService.userIdentity.login,
+        3
+      );
+    } else {
+      return new profileData4('', '', '', '', -1, '', 0);
     }
   }
 }
