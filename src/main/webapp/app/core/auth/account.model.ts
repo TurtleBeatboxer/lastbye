f5fd570c999ef9fd06a3627d4f77bba7c467ec5a
Koczokok <p.kurylowicz@outlook.com>
2023-06-26 08:42:22 +0200

    stepper

--- src/main/webapp/app/core/auth/account.model.ts
+++ src/main/webapp/app/core/auth/account.model.ts
@@ -8,7 +8,6 @@ export class Account {
     public langKey: string,
     public lastName: string | null,
     public login: string,
-    public imageUrl: string | null,
-    public levelOfForm: number
+    public imageUrl: string | null
   ) {}
 }
