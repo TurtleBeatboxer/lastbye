2c3254546bb95a0b680003535b423680793e907b
Koczokok <p.kurylowicz@outlook.com>
2023-06-27 00:25:12 +0200

    level detector

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -38,6 +38,7 @@ export class SettingsComponent implements OnInit {
     imageUrl: new FormControl(initialAccount.imageUrl, { nonNullable: true }),
     login: new FormControl(initialAccount.login, { nonNullable: true }),
     userId: new FormControl(initialAccount.userId, { nonNullable: true }),
+    levelOfForm: new FormControl(initialAccount.levelOfForm, { nonNullable: true }),
   });
 
   constructor(
