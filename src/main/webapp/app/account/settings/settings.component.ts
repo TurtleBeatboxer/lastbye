27b7b71ac75a172b4616ac7ccdf280d037a7be43
Koczokok <p.kurylowicz@outlook.com>
2023-07-10 23:01:29 +0200

    SAYB-29

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -8,7 +8,7 @@ import { LANGUAGES } from 'app/config/language.constants';
 import { HttpClient } from '@angular/common/http';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 
-const initialAccount: Account = {} as Account;
+// const this.initialAccount: Account = {} as Account;
 
 @Component({
   selector: 'jhi-settings',
@@ -20,21 +20,22 @@ export class SettingsComponent implements OnInit {
   languages = LANGUAGES;
   isTrueCoffin: boolean;
   editsError = false;
-
+  initialAccount: Account = {} as Account;
+  edits: number;
   settingsForm = new FormGroup({
-    firstName: new FormControl(initialAccount.firstName, {
+    firstName: new FormControl(this.initialAccount.firstName, {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(1), Validators.maxLength(50)],
     }),
-    lastName: new FormControl(initialAccount.lastName, {
+    lastName: new FormControl(this.initialAccount.lastName, {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(1), Validators.maxLength(50)],
     }),
-    email: new FormControl(initialAccount.email, {
+    email: new FormControl(this.initialAccount.email, {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email],
     }),
-    prefix: new FormControl(initialAccount.prefix, {
+    prefix: new FormControl(this.initialAccount.prefix, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -43,7 +44,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    phone: new FormControl<number>(initialAccount.phone, {
+    phone: new FormControl<number>(this.initialAccount.phone, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -52,7 +53,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    burialMethod: new FormControl(initialAccount.burialMethod, {
+    burialMethod: new FormControl(this.initialAccount.burialMethod, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -61,7 +62,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    graveInscription: new FormControl(initialAccount.graveInscription, {
+    graveInscription: new FormControl(this.initialAccount.graveInscription, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -70,8 +71,8 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    openCoffin: new FormControl(initialAccount.openCoffin, { nonNullable: true, validators: [Validators.required] }),
-    clothes: new FormControl(initialAccount.clothes, {
+    openCoffin: new FormControl(this.initialAccount.openCoffin, { nonNullable: true, validators: [Validators.required] }),
+    clothes: new FormControl(this.initialAccount.clothes, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -80,11 +81,11 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    flowers: new FormControl(initialAccount.flowers, {
+    flowers: new FormControl(this.initialAccount.flowers, {
       nonNullable: true,
       validators: [Validators.required],
     }),
-    ifFlowers: new FormControl(initialAccount.ifFlowers, {
+    ifFlowers: new FormControl(this.initialAccount.ifFlowers, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -93,7 +94,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    obituary: new FormControl(initialAccount.obituary, {
+    obituary: new FormControl(this.initialAccount.obituary, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -102,7 +103,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    spotify: new FormControl(initialAccount.spotify, {
+    spotify: new FormControl(this.initialAccount.spotify, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -111,7 +112,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    guests: new FormControl(initialAccount.guests, {
+    guests: new FormControl(this.initialAccount.guests, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -120,7 +121,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    notInvited: new FormControl(initialAccount.notInvited, {
+    notInvited: new FormControl(this.initialAccount.notInvited, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -129,7 +130,7 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    placeOfCeremony: new FormControl(initialAccount.placeOfCeremony, {
+    placeOfCeremony: new FormControl(this.initialAccount.placeOfCeremony, {
       nonNullable: true,
       validators: [
         Validators.required,
@@ -138,34 +139,35 @@ export class SettingsComponent implements OnInit {
         Validators.pattern('^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$'),
       ],
     }),
-    farewellLetter: new FormControl(initialAccount.farewellLetter, { nonNullable: true, validators: [Validators.required] }),
-    videoSpeech: new FormControl(initialAccount.videoSpeech, { nonNullable: true, validators: [Validators.required] }),
-    testament: new FormControl(initialAccount.testament, { nonNullable: true, validators: [Validators.required] }),
-    other: new FormControl(initialAccount.other, { nonNullable: true, validators: [Validators.required] }),
-    authorities: new FormControl(initialAccount.authorities, { nonNullable: true }),
-    imageUrl: new FormControl(initialAccount.imageUrl, { nonNullable: true }),
-    login: new FormControl(initialAccount.login, { nonNullable: true }),
-    userId: new FormControl(initialAccount.userId, { nonNullable: true }),
-    langKey: new FormControl(initialAccount.langKey, { nonNullable: true }),
-    levelOfForm: new FormControl(initialAccount.levelOfForm, { nonNullable: true }),
-    activated: new FormControl(initialAccount.activated, { nonNullable: true }),
-    editsLeft: new FormControl(initialAccount.editsLeft, { nonNullable: true }),
+    farewellLetter: new FormControl(this.initialAccount.farewellLetter, { nonNullable: true, validators: [Validators.required] }),
+    videoSpeech: new FormControl(this.initialAccount.videoSpeech, { nonNullable: true, validators: [Validators.required] }),
+    testament: new FormControl(this.initialAccount.testament, { nonNullable: true, validators: [Validators.required] }),
+    other: new FormControl(this.initialAccount.other, { nonNullable: true, validators: [Validators.required] }),
+    authorities: new FormControl(this.initialAccount.authorities, { nonNullable: true }),
+    imageUrl: new FormControl(this.initialAccount.imageUrl, { nonNullable: true }),
+    login: new FormControl(this.initialAccount.login, { nonNullable: true }),
+    userId: new FormControl(this.initialAccount.userId, { nonNullable: true }),
+    langKey: new FormControl(this.initialAccount.langKey, { nonNullable: true }),
+    levelOfForm: new FormControl(this.initialAccount.levelOfForm, { nonNullable: true }),
+    activated: new FormControl(this.initialAccount.activated, { nonNullable: true }),
+    editsLeft: new FormControl(this.initialAccount.editsLeft, { nonNullable: true }),
   });
 
   constructor(
-    private accountService: AccountService,
+    public accountService: AccountService,
     private translateService: TranslateService,
-    private http: HttpClient,
+
     private applicationConfigService: ApplicationConfigService
   ) {}
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => {
       if (account) {
+        this.edits = account.editsLeft;
         this.settingsForm.patchValue(account);
-        this.isTrueCoffin = true;
+
         console.log(this.isTrueCoffin);
-        console.log(initialAccount);
+        console.log(this.initialAccount);
       }
     });
   }
@@ -188,5 +190,8 @@ export class SettingsComponent implements OnInit {
         this.translateService.use(account.langKey);
       }
     });
+  }
+  onClick() {
+    console.log(this.initialAccount);
   }
 }
