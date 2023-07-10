976e9178e80474082d87c65fd45ac7dc537bec61
Koczokok <p.kurylowicz@outlook.com>
2023-07-10 22:45:49 +0200

    refactor

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { NbCardModule, NbDatepickerModule, NbInputModule, NbLayoutModule } from '@nebular/theme';
+import { NavbarComponent } from 'app/layouts/navbar/navbar.component';
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), NbCardModule, NbDatepickerModule, NbInputModule, NbLayoutModule],
   declarations: [HomeComponent],
