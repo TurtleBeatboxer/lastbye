d1fee731b96c8643dbea5e3029a7e411ca5e64eb
mmarzecki <mmarzecki2@finitec.pl>
2023-09-26 14:44:37 +0200

    added-getImage-backend

--- src/main/java/com/origami/service/FileService.java
+++ src/main/java/com/origami/service/FileService.java
@@ -8,14 +8,14 @@ import com.origami.repository.FilesRepository;
 import com.origami.repository.ProfileRepository;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileService {
-
-    public FileService() {}
 
     @Autowired
     private FilesRepository filesRepository;
@@ -26,19 +26,35 @@ public class FileService {
     @Autowired
     private ProfileRepository profileRepository;
 
+    private final String SEPARATOR = FileSystems.getDefault().getSeparator();
+
     public String uploadImage(FileDTO fileDTO) throws IOException {
-        Files fileData = new Files();
-        fileData.setName(fileDTO.getFile().getOriginalFilename());
-        fileData.setFormat(fileDTO.getFile().getContentType());
-        fileData.setFileType(stringToFileType(fileDTO.getType()));
-        Profile profile = profileRepository.findOneByUserId(fileDTO.getUserId()).get();
-        fileData.setProfile(profile);
-        String filePath = FOLDER_PATH + profile.getUserId() + "/" + fileDTO.getFile().getOriginalFilename();
-        filesRepository.save(fileData);
-        fileData.setFilePath(filePath);
-        fileDTO.getFile().transferTo(new File(filePath));
-        if (fileData != null) {
-            return "file uploaded successfully : " + filePath;
+        Optional<Profile> profileOptional = profileRepository.findOneByUserId(fileDTO.getUserId());
+        if (profileOptional.isPresent()) {
+            Profile profile = profileOptional.get();
+            if (!profile.isFinishedEditing()) {
+                Files fileData = new Files();
+                fileData.setName(fileDTO.getFile().getOriginalFilename());
+                fileData.setFormat(fileDTO.getFile().getContentType());
+                fileData.setFileType(stringToFileType(fileDTO.getType()));
+                fileData.setProfile(profile);
+                String filePath = FOLDER_PATH + profile.getUserId() + SEPARATOR + fileDTO.getType();
+                filesRepository.save(fileData);
+                fileData.setFilePath(filePath);
+                fileDTO.getFile().transferTo(new File(filePath));
+                if (fileData != null) {
+                    return "file uploaded successfully : " + filePath;
+                }
+            }
+        }
+        return null;
+    }
+
+    public File downloadPublicProfileImage(String publicProfile) {
+        Optional<Profile> optionalProfile = profileRepository.findProfileByPublicProfileLink(publicProfile);
+        if (optionalProfile.isPresent()) {
+            Profile profile = optionalProfile.get();
+            return new File(FOLDER_PATH + profile.getUserId() + SEPARATOR + "publicPicture");
         }
         return null;
     }
@@ -55,6 +71,22 @@ public class FileService {
                 return FileType.PUBLIC_PICTURE;
             case ("videoSpeech"):
                 return FileType.VIDEO_SPEECH;
+        }
+        return null;
+    }
+
+    public String fileTypeToFileName(FileType fileType) {
+        switch (fileType) {
+            case BURIAL_PICTURE:
+                return "burialPicture";
+            case FAREWELL_LETTER:
+                return "farewellLetter";
+            case TESTAMENT:
+                return "testament";
+            case PUBLIC_PICTURE:
+                return "publicPicture";
+            case VIDEO_SPEECH:
+                return "videoSpeech";
         }
         return null;
     }
