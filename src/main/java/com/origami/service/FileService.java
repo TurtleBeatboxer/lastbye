eb7dd3139d9438b5aaa39e86b4783e87fc0319c5
mmarzecki <mmarzecki2@finitec.pl>
2023-09-26 12:51:55 +0200

    code-refactor

--- src/main/java/com/origami/service/FileService.java
+++ src/main/java/com/origami/service/FileService.java
@@ -2,14 +2,15 @@ package com.origami.service;
 
 import com.origami.domain.Files;
 import com.origami.domain.Profile;
+import com.origami.domain.dtos.FileDTO;
 import com.origami.domain.enumeration.FileType;
 import com.origami.repository.FilesRepository;
 import com.origami.repository.ProfileRepository;
 import java.io.File;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileService {
@@ -19,23 +20,23 @@ public class FileService {
     @Autowired
     private FilesRepository filesRepository;
 
-    private final String FOLDER_PATH = "/Users/pkury/Desktop/lastgoodbuyImage/";
+    @Value("${files.path}")
+    private String FOLDER_PATH;
 
     @Autowired
     private ProfileRepository profileRepository;
 
-    public String uploadImage(MultipartFile file, String type, String userId) throws IOException {
+    public String uploadImage(FileDTO fileDTO) throws IOException {
         Files fileData = new Files();
-
-        fileData.setName(file.getOriginalFilename());
-        fileData.setFormat(file.getContentType());
-        fileData.setFileType(stringToFileType(type));
-        Profile profile = profileRepository.findOneByUserId((long) Integer.parseInt(userId)).get();
+        fileData.setName(fileDTO.getFile().getOriginalFilename());
+        fileData.setFormat(fileDTO.getFile().getContentType());
+        fileData.setFileType(stringToFileType(fileDTO.getType()));
+        Profile profile = profileRepository.findOneByUserId(fileDTO.getUserId()).get();
         fileData.setProfile(profile);
-        String filePath = FOLDER_PATH + profile.getUserId() + "/" + file.getOriginalFilename();
+        String filePath = FOLDER_PATH + profile.getUserId() + "/" + fileDTO.getFile().getOriginalFilename();
         filesRepository.save(fileData);
         fileData.setFilePath(filePath);
-        file.transferTo(new File(filePath));
+        fileDTO.getFile().transferTo(new File(filePath));
         if (fileData != null) {
             return "file uploaded successfully : " + filePath;
         }
