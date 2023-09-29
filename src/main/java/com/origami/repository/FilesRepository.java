930afd5aa934f64be85a37037baab423bbee48a1
mmarzecki <mmarzecki2@finitec.pl>
2023-09-29 17:47:05 +0200

    severe-refactoring

--- src/main/java/com/origami/repository/FilesRepository.java
+++ src/main/java/com/origami/repository/FilesRepository.java
@@ -2,6 +2,7 @@ package com.origami.repository;
 
 import com.origami.domain.Files;
 import com.origami.domain.Profile;
+import java.util.ArrayList;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FilesRepository extends JpaRepository<Files, Long> {
     Optional<Files> findOneByProfileId(Long profileId);
+    ArrayList<Files> findAllByProfileId(Long profileId);
 }
